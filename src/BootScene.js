5cccd85dbb2ef329bb8b897c3704bf394df1f04d
mr-35843 <mr-35843@proton.me>
2023-11-22 12:39:10 +0100

    quick fix

--- src/BootScene.js
+++ src/BootScene.js
@@ -11,7 +11,7 @@ export default class BootScene extends Phaser.Scene {
 		//load/cache tileset images
 		this.load.image('build_atlas', 'assets/tilesets/build_atlas.png');
 		this.load.image('lpc_forest_tiles','assets/tilesets/lpc_forest_tiles.png');
-		this.load.image('room_builder','assets/tilesets/room_builder.png');
+		this.load.image('terrain_01','assets/tilesets/terrain.png');
 		this.load.image('terrain_atlas','assets/tilesets/terrain_atlas.png');
 
 		// load/cache maps
