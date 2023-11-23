708bf5b48d7f424bc7e9e149159db5685e69da7b
mr-35843 <mr-35843@proton.me>
2023-11-23 12:30:18 +0100

    Seperating settings from main

--- src/main.js
+++ src/main.js
@@ -1,51 +1,23 @@
 import Phaser from 'phaser'
 import BootScene from './scenes/BootScene'
 import Build0002 from './scenes/Build0002';
+import SettingsScreenResolution from './middleware/settings/ScreenResolution';
 
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
-// @ts-ignore
-var screenResolution = document.getElementById("screenResolution").value;
-console.log(screenResolution);
-var zoom = 1; //default.
-const zoomLargeScreenMultiplyer = 1.5;
+let appSettingsResolution = new SettingsScreenResolution();
 
-switch(screenResolution){
 
-	case "720" || 720:
-		zoom = zoom * zoomLargeScreenMultiplyer;
-		windowHeight = 720 / zoom;
-		windowWidth = 1080 / zoom;
-		windowWidth = windowWidth * 0.95;
-		windowHeight = windowHeight * 0.9;
-		break;
 
-	case "360" || 360:
-		windowHeight = 360;
-		windowWidth = 480;
-		break;
 
-	default:
-		zoom = zoom * zoomLargeScreenMultiplyer;
-			windowHeight = windowHeight / zoom;
-			windowWidth = windowWidth / zoom;
-			windowWidth = windowWidth * 0.95;
-			windowHeight = windowHeight * 0.9;
-		break;
-}
 
-console.log(windowHeight);
-console.log(windowWidth);
-//DEBUG:
-//console.log(windowWidth + " width.");
-//console.log(windowHeight + " height.");
+
+
 
 const config = {
 	type: Phaser.AUTO,
 	parent: 'app',
-	width: windowWidth,
-	height: windowHeight,
-	zoom: zoom,
+	width: appSettingsResolution.windowWidth,
+	height: appSettingsResolution.windowHeight,
+	zoom: appSettingsResolution.zoom,
 	physics: {
 		default: 'arcade',
 		arcade: {
---
+++ src/middleware/settings/ScreenResolution.js
@@ -0,0 +1,63 @@
+export default class SettingsScreenResolution {
+    constructor(){
+        this.windowHeight = window.innerHeight;
+        this.windowWidth = window.innerWidth;
+        // @ts-ignore depends on the element/ID of screenResolution
+        this.windowResolution = document.getElementById("screenResolution").value;
+        this.zoom = 1;
+        this.zoomLargeScreenMultiplyer = 1.5;
+
+
+        switch(this.windowResolution){
+
+            case "720" || 720:
+                this.scale720();
+                break;
+        
+            case "360" || 360:
+                console.log("pre 360")
+                this.scale360()
+                break;
+        
+            default:
+                this.scaleAutoFill();
+                break;
+        }
+    }
+
+    scaleAppWindowLargeScreens(){
+        this.windowWidth = this.windowWidth * 0.95;
+        this.windowHeight = this.windowHeight * 0.9;
+    }
+    
+    scaleAutoFill(){
+    
+        if(this.windowHeight > 720){
+            this.zoom = this.zoom * this.zoomLargeScreenMultiplyer;
+            this.windowHeight = this.windowHeight / this.zoom;
+            this.windowWidth = this.windowWidth / this.zoom;
+            this.scaleAppWindowLargeScreens();
+        } else {
+            this.windowHeight = this.windowHeight * this.zoom;
+            this.windowWidth = this.windowWidth * this.zoom;
+        }
+        
+    }
+    
+    scale360(){
+        this.windowHeight = 360;
+        this.windowWidth = 480;
+    }
+    
+    scale720(){
+        this.zoom = this.zoom * this.zoomLargeScreenMultiplyer;
+        this.windowHeight = 720 / this.zoom;
+        this.windowWidth = 1080 / this.zoom;
+        this.scaleAppWindowLargeScreens();
+    }
+
+    
+
+
+    
+}
