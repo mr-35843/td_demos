92216204584c1e312ce976fc19da705d37937a2d
mr-35843 <mr-35843@proton.me>
2023-11-24 12:42:45 +0100

    game client now resizes on input

--- src/main.js
+++ src/main.js
@@ -3,21 +3,32 @@ import BootScene from './scenes/BootScene'
 import Build0002 from './scenes/Build0002';
 import SettingsScreenResolution from './middleware/settings/ScreenResolution';
 
-let appSettingsResolution = new SettingsScreenResolution();
-const config = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: appSettingsResolution.windowWidth,
-	height: appSettingsResolution.windowHeight,
-	zoom: appSettingsResolution.zoom,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 0 },
+let restartTriggerSelector = document.getElementById("screenResolution");
+restartTriggerSelector.addEventListener("change", ()=>{
+	document.getElementById("app").innerHTML = "";
+	startGame();
+})
+
+startGame();
+
+function startGame(){
+	let appSettingsResolution = new SettingsScreenResolution();
+	const config = {
+		type: Phaser.AUTO,
+		parent: 'app',
+		width: appSettingsResolution.windowWidth,
+		height: appSettingsResolution.windowHeight,
+		zoom: appSettingsResolution.zoom,
+		physics: {
+			default: 'arcade',
+			arcade: {
+				gravity: { y: 0 },
+			},
+			
 		},
-		
-	},
-	scene: [BootScene, Build0002],
+		scene: [BootScene, Build0002],
+	}
+
+	return new Phaser.Game(config);
 }
 
-export default new Phaser.Game(config)
